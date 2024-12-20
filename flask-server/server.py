8ff6f426de41c1d56eddab8f3df8410775628d76
Vincent Yunansan <140985708+vyun8699@users.noreply.github.com>
2024-12-20 12:52:51 +1100

    commit

--- flask-server/server.py
+++ flask-server/server.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 CORS(app, resources={
     r"/*": {
-        "origins": ["http://localhost:3000", "http://localhost:3001", "http://localhost:5001","https://vyun8699.github.io"],
+        "origins": ["http://localhost:3000", "http://localhost:3001", "http://localhost:5001","https://vyun8699.github.io","https://nocodeeda.onrender.com"],
         "methods": ["GET", "POST"],
         "allow_headers": ["Content-Type"]
     }
